81787455a7e5f10709b54ed1d4751cf36f32d0f4
Kuba <ksulaymanov@gmail.com>
2022-07-07 01:41:46 -0500

    added functionality to create new task

--- djangohomework45/urls.py
+++ djangohomework45/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from todo.views import index_view
+from todo.views import index_view, add_task
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view)
+    path('', index_view),
+    path('add_task/', add_task)
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-from todo.models import Task
+from todo.models import Task, STATUS_CHOICES
 
 
 def index_view(request):
@@ -10,5 +10,14 @@ def index_view(request):
     return render(request, 'index.html', context)
 
 
-##def tasks(request):
+def add_task(request):
+    if request.method == 'GET':
+        return render(request, 'add.html', {'statuses': STATUS_CHOICES})
+    else:
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        deadline = request.POST.get('deadline')
+        Task.objects.create(description=description, status=status, deadline=deadline)
+        return render(request, 'add.html')
+
 
